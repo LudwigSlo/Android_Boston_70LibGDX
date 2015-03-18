ea233e9c9c8e54372d49df04a2ce2aee57e10347
LudwigSlo <ludwig_slotte@hotmail.com>
2015-03-18 19:53:48 +0100

    Initial commit

---
+++ core/src/com/example/ludwig/Boston70libGDXGame/MyGdxGame.java
@@ -0,0 +1,47 @@
+package com.example.ludwig.Boston70libGDXGame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends ApplicationAdapter {
+    private SpriteBatch batch;
+    private BitmapFont font;
+    private Texture img;
+    private Sprite sprite;
+
+
+	
+	@Override
+	public void create () {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.setColor(Color.BLUE);
+        font.scale(5);
+	}
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+
+    @Override
+	public void render () {
+
+        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        font.draw(batch, "Hey this is almost a game now, right?!", 100, 400);
+
+        batch.end();
+
+
+	}
+}
